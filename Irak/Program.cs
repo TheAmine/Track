ab078d064bc6aaf4ee866d8e2b4829ae31260708
Amine <mohamed-amine.touchene@etu.univ-lehavre.fr>
2018-05-03 14:41:50 +0200

    Add +

--- Irak/Program.cs
+++ Irak/Program.cs
@@ -7,7 +7,14 @@ namespace Irak
         static void Main(string[] args)
         {
             // Ad comment befor commit 
+
+            int a = 1;
+            int b = 2;
+     
             Console.WriteLine("Hello Irak!");
+            Console.WriteLine(a+b);
+
+
         }
     }
 }
